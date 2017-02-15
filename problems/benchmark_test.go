7256428f796996a0e96947c7a8125e0d7293cce1
Ian Ferguson <ian.ferguson@stratagen.com>
2017-02-14 22:24:04 -0800

    Added unit tests for regression testing and added benchmark tests

---
+++ problems/benchmark_test.go
@@ -0,0 +1,313 @@
+package problems
+
+import (
+    "testing"
+)
+
+func BenchmarkProblem0001 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[1].Solve()
+    }
+}
+
+func BenchmarkProblem0002 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[2].Solve()
+    }
+}
+
+func BenchmarkProblem0003 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[3].Solve()
+    }
+}
+
+func BenchmarkProblem0004 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[4].Solve()
+    }
+}
+
+func BenchmarkProblem0005 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[5].Solve()
+    }
+}
+
+func BenchmarkProblem0006 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[6].Solve()
+    }
+}
+
+func BenchmarkProblem0007 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[7].Solve()
+    }
+}
+
+func BenchmarkProblem0008 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[8].Solve()
+    }
+}
+
+func BenchmarkProblem0009 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[9].Solve()
+    }
+}
+
+func BenchmarkProblem0010 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[10].Solve()
+    }
+}
+
+func BenchmarkProblem0011 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[11].Solve()
+    }
+}
+
+func BenchmarkProblem0012 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[12].Solve()
+    }
+}
+
+func BenchmarkProblem0013 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[13].Solve()
+    }
+}
+
+func BenchmarkProblem0014 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[14].Solve()
+    }
+}
+
+func BenchmarkProblem0015 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[15].Solve()
+    }
+}
+
+func BenchmarkProblem0016 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[16].Solve()
+    }
+}
+
+func BenchmarkProblem0017 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[17].Solve()
+    }
+}
+
+func BenchmarkProblem0018 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[18].Solve()
+    }
+}
+
+func BenchmarkProblem0019 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[19].Solve()
+    }
+}
+
+func BenchmarkProblem0020 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[20].Solve()
+    }
+}
+
+func BenchmarkProblem0021 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[21].Solve()
+    }
+}
+
+func BenchmarkProblem0022 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[22].Solve()
+    }
+}
+
+func BenchmarkProblem0023 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[23].Solve()
+    }
+}
+
+func BenchmarkProblem0024 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[24].Solve()
+    }
+}
+
+func BenchmarkProblem0025 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[25].Solve()
+    }
+}
+
+func BenchmarkProblem0026 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[26].Solve()
+    }
+}
+
+func BenchmarkProblem0027 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[27].Solve()
+    }
+}
+
+func BenchmarkProblem0028 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[28].Solve()
+    }
+}
+
+func BenchmarkProblem0029 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[29].Solve()
+    }
+}
+
+func BenchmarkProblem0030 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[30].Solve()
+    }
+}
+
+func BenchmarkProblem0031 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[31].Solve()
+    }
+}
+
+func BenchmarkProblem0032 (b *testing.B) {
+    b.Skip("Not implemented")
+}
+
+func BenchmarkProblem0033 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[33].Solve()
+    }
+}
+
+func BenchmarkProblem0034 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[34].Solve()
+    }
+}
+
+func BenchmarkProblem0035 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[35].Solve()
+    }
+}
+
+func BenchmarkProblem0036 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[36].Solve()
+    }
+}
+
+func BenchmarkProblem0037 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[37].Solve()
+    }
+}
+
+func BenchmarkProblem0038 (b *testing.B) {
+    b.Skip("Not implemented")
+}
+
+func BenchmarkProblem0039 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[39].Solve()
+    }
+}
+
+func BenchmarkProblem0040 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[40].Solve()
+    }
+}
+
+func BenchmarkProblem0041 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[41].Solve()
+    }
+}
+
+func BenchmarkProblem0042 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[42].Solve()
+    }
+}
+
+func BenchmarkProblem0043 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[43].Solve()
+    }
+}
+
+func BenchmarkProblem0044 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[44].Solve()
+    }
+}
+
+func BenchmarkProblem0045 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[45].Solve()
+    }
+}
+
+func BenchmarkProblem0046 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[46].Solve()
+    }
+}
+
+func BenchmarkProblem0047 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[47].Solve()
+    }
+}
+
+func BenchmarkProblem0048 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[48].Solve()
+    }
+}
+
+func BenchmarkProblem0049 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[49].Solve()
+    }
+}
+
+func BenchmarkProblem0050 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[50].Solve()
+    }
+}
+
+func BenchmarkProblem0067 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[67].Solve()
+    }
+}
+
+func BenchmarkProblem0097 (b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        Registry[97].Solve()
+    }
+}
